c82141169f1c9bb0f65c44c233b38305e7f21d25
mrmrs <hi@mrmrs.cc>
2016-04-13 12:19:46 +0100

    More responsive list component with some simple spacing.

--- components/collections/ListCollection.jsx
+++ components/collections/ListCollection.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 const ListCollection = ({ items, ...props }) => (
   <section data-name='ListCollection'>
     <h2 className='black' children='title' />
-    <ul className='black'>
+    <ul className='list pl0 ml0'>
       {items.map((item, i) => (
-        <li key={i} className='black'>
+        <li key={i} className='w-100 pv3'>
+          <img src={item.source.cloudinary_id} className='db w-100 mw-100' />
           <span children={item.source.title} className='db b'/>
-          <img src={item.source.cloudinary_id} className='db'/>
         </li>
       ))}
     </ul>
